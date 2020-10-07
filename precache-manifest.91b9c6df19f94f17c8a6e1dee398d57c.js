2989117ebade10ccb6c80a4a5fed2613dc3e3a5c
Wasif Baliyan <w.baliyan@gmail.com>
2020-10-07 20:33:08 +0530

    Updates

---
+++ precache-manifest.91b9c6df19f94f17c8a6e1dee398d57c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea08128af7fa674fd91185080e0e0634",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "f0e004b899339e066ac3",
+    "url": "/smart-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "374f2ee1a7be36046997",
+    "url": "/smart-brain/static/css/main.c58f872c.chunk.css"
+  },
+  {
+    "revision": "f0e004b899339e066ac3",
+    "url": "/smart-brain/static/js/2.8b237a3d.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smart-brain/static/js/2.8b237a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "374f2ee1a7be36046997",
+    "url": "/smart-brain/static/js/main.6ad93f43.chunk.js"
+  },
+  {
+    "revision": "5f284a9b7132a669a655",
+    "url": "/smart-brain/static/js/runtime-main.43daafc1.js"
+  }
+]);
